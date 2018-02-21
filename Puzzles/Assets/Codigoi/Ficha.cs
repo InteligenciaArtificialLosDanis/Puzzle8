0b7e67adfebf6864969013997ec443dcccc7e2b9
Usuario Local <Usuario Local>
2018-02-21 19:00:17 +0100

    Vuelta al pasado y no se cuelga

---
+++ Puzzles/Assets/Codigoi/Ficha.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ficha : MonoBehaviour {
+   public GameObject pieza;
+    public GameObject empty;
+    public GameObject gm; //Game Manager
+
+    //public int id;
+
+    int matrizX, matrizY;
+
+	public int PosCorrectaY;
+	public int PosCorrectaX;
+
+
+    //float piezaX, piezaY, piezaZ;
+    Vector3 posPieza;
+   
+    // Use this for initialization
+    void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnMouseDown()
+    {
+        //if (gm.GetComponent<GameManager>().movimientoLegal(pieza))
+        //{
+            posPieza = pieza.transform.position;
+            pieza.transform.position = empty.transform.position;
+            empty.transform.position = posPieza;
+        //}
+    }
+
+	bool bienPuesto (int x, int y){
+		return (x == PosCorrectaX && y == PosCorrectaY);
+	}
+
+	public void setPosMatriz (int x, int y){
+
+		matrizX = x;
+		matrizY = y;
+
+	}
+}
+
---
+++ Puzzles/Assets/Codigoi/GameManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour {
+    public GameObject[] Fichas;
+	public GameObject empty;
+
+    public GameObject[,] tablero;
+
+    bool[] usados = new bool[8];
+	bool fin = false;
+
+
+    // Use this for initialization
+    void Start () {
+        tablero = new GameObject[3, 3];
+
+        int i;			//Itera sobre fichas
+        int y = 2;		//Posicion y
+        int x = -4;		//Posicion X
+
+        for (int f = 0; f < 3; f++) //FILS
+        {
+            for (int c = 0; c < 3; c++) //COLS
+            {
+				if (f == 2 && c == 2) {  
+					tablero [f, c] = Instantiate (empty, new Vector3 (x, y, 0), Quaternion.identity) as GameObject;
+                    empty.GetComponent<Ficha>().setPosMatriz(f, c);
+                }
+
+                do
+                {
+                    i = Random.Range(0, 8);
+                }
+                while (usados[i] == true);
+
+                tablero[f, c] = Instantiate(Fichas[i], new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                usados[i] = true;
+
+                Fichas[i].GetComponent<Ficha>().setPosMatriz(f, c);
+
+                x = x + 2;  //Movemos la X
+            }
+            x = -4;         //Restauramos la X
+            y = y - 2;      //Bajamos la fila
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*bool movimientoLegal(GameObject ficha){
+        //Si la partida no se ha acabado
+
+        if (fin)
+            return false;
+        //Pregunta si hay un gameobject con tag empty, y si lo hay devuelve true para mover la ficha
+        else
+        {
+            int fila = ficha.GetComponent<Ficha>().getMatrizY();
+            int columna = ficha.GetComponent<Ficha>().getMatrizX();
+            //Arriba
+            if (fila - 1 >= 0 && tablero[fila - 1, columna] == empty)
+            {
+                swapPosMatriz(ficha);
+                return true;
+            }
+            //Abajo
+            if (fila + 1 <= 2 && tablero[fila + 1, columna] == empty)
+            {
+                swapPosMatriz(ficha);
+                return true;
+            }
+            //Derecha
+            if (columna + 1 <= 2 && tablero[fila, columna + 1] == empty)
+            {
+                swapPosMatriz(ficha);
+                return true;
+            }
+            //Izquierda
+            if (columna - 1 >= 0 && tablero[fila, columna - 1] == empty)
+            {
+                swapPosMatriz(ficha);
+                return true;
+            }
+
+            //Si a su alrededor no hay vacío, no puedes mover esa ficha
+            return false;
+        }
+
+
+    }*/
+
+    /*void swapPosMatriz(GameObject ficha)
+    {
+        //Guardo la posicion de ficha. Le asigno la posicion de empty a la ficha
+        //Después, meto la posicion guardada de la ficha al empty.
+        int fichaF = ficha.GetComponent<Ficha>().getMatrizY();
+        int fichaC = ficha.GetComponent<Ficha>().getMatrizX();
+
+        ficha.GetComponent<Ficha>().setPosMatriz(empty.GetComponent<Empty>().matrizX, empty.GetComponent<Empty>().matrizY);
+
+        empty.GetComponent<Empty>().setPosMatriz(fichaC, fichaF);
+    }*/
+
+
+    void partidaAcabada() {
+		//Comprueba todas las fichas a ver si están bien puestas :) (while)
+
+		//If (todas) cout << YAAAAAY :3
+		//else no.
+
+
+	}
+}
